ba8665dcca95292980244718efa3fad3cdb4989c
Andrew Smith <andrew@andrew.codes>
2016-10-18 12:01:07 -0400

    Shore up specs; use enzyme

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,19 @@
-const {jsdom} = require('jsdom');
-global.document = jsdom('<html><body></body></html>');
+var chai = require('chai');
+var chaiEnzyme = require('chai-enzyme');
+
+chai.use(chaiEnzyme());
+global.should = chai.should();
+
+var jsdom = require('jsdom').jsdom;
+var exposedProperties = ['window', 'navigator', 'document'];
+global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+    if (typeof global[property] === 'undefined') {
+        exposedProperties.push(property);
+        global[property] = document.defaultView[property];
+    }
+});
 global.navigator = {
-  userAgent: 'node.js'
+    userAgent: 'node.js'
 };
---
+++ test/todo-item-test.spec.js
@@ -0,0 +1,21 @@
+const React = require('react');
+const {mount} = require('enzyme');
+const TodoItem = require('../Lab13/todo-item');
+
+describe('Todo-item component', function() {
+    describe('when rendering the component', () => {
+        beforeEach(() => {
+            this.actual = mount(
+                <TodoItem done={false} name="Write Tutorial" />
+            );
+        });
+
+        it('it should render an <input> should be of type "checkbox"', () => {
+            this.actual.find('input').should.have.attr('type', 'checkbox');
+        });
+
+        it('it should render as unchecked', () => {
+            this.actual.find('input').should.not.have.attr('checked', true);
+        });
+    });
+});
